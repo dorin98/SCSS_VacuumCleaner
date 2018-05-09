7aef1a84b11ca7761ada74aac51a3116208daf19
Dorin Andrei Geman <33001019+dorin98@users.noreply.github.com>
2018-05-09 23:54:10 +0300

    Add files via upload

---
+++ IR.ino
@@ -0,0 +1,27 @@
+#include <IRremote.h>
+
+#define UP 16718055
+#define DOWN 16730805
+#define RIGHT 16734885
+#define LEFT 16716015
+
+int pin = 4;
+IRrecv irrecv(pin);
+decode_results results;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  Serial.print("Welcome");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (irrecv.decode(&results)) {
+    Serial.print("Value: ");
+    Serial.print(results.value, DEC);
+    Serial.print("\n");
+    irrecv.resume(); // Receive the next value
+  }
+}
