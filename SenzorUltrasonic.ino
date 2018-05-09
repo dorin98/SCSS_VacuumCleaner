9c171cabbdbc9aafb4b34a451a306d69a1c52be9
Dorin Andrei Geman <33001019+dorin98@users.noreply.github.com>
2018-05-09 23:54:27 +0300

    Add files via upload

---
+++ SenzorUltrasonic.ino
@@ -0,0 +1,29 @@
+const int trigPin = 13;
+const int echoPin = 12;
+
+long duration;
+long distance;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(1000);
+}
